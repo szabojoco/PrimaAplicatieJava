37ffe992f1f919885b5877192778a282a0b1b99a
Szabo Joco <szabojoco@yahoo.com>
2022-01-25 08:26:42 +0200

    Upload

---
+++ src/ro/java/ctrln/Main.java
@@ -0,0 +1,10 @@
+package ro.java.ctrln;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Jar - Java Arive
+        // JAVA_HOME
+	    System.out.println("Invat limbajul Java si despre masina virtuala Java!");
+    }
+}
