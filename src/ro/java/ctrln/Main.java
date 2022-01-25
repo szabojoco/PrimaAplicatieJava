3eea291e494ac413a64e19327b7845b3f1a77af4
Szabo Joco <szabojoco@yahoo.com>
2022-01-25 09:16:22 +0200

    Modificare Main.java

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -3,7 +3,6 @@ package ro.java.ctrln;
 public class Main {
 
     public static void main(String[] args) {
-        // Jar - Java Arive
         // JAVA_HOME
 	    System.out.println("Invat limbajul Java si despre masina virtuala Java!");
     }
