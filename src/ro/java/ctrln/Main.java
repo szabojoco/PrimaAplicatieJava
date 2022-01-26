5f00079795a0c68b879f2d4fbfdb0ba946d992f5
Szabo Joco <szabojoco@yahoo.com>
2022-01-26 13:17:07 +0200

    upgrade Main.java

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         // JAVA_HOME
-        
+        //
 	    System.out.println("Invat limbajul Java si despre masina virtuala Java!");
     }
 }
