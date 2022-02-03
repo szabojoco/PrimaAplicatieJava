11f8aab54c7541cd09bdc939da3bb45631a610e8
szabojoco <szabojoco@yahoo.com>
2022-02-03 09:22:09 +0200

    Am adaugat un single line comment

---
+++ src/ro/java/misc/Main.java
@@ -0,0 +1,10 @@
+package ro.java.misc;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // single line comment
+        System.out.println("Salut din ro.java.misc.Main");
+    }
+}
